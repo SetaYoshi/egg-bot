906e8b5885dcd3d76eb56120a1c27dd5e480d7fb
Thomas Shuttle <t.hyaumet@free.fr>
2022-08-09 18:03:19 +0200

    add video support, fixes

--- libs/commands/nasa.lua
+++ libs/commands/nasa.lua
@@ -17,10 +17,10 @@ local appID = require("files/token").NASA
 
 -- Request an apod object for today. If a date is specified, then return the APOD for that day
 local function requestAPOD(date)
-  local url = baseURL.."?api_key="..appID
+  local url = baseURL .. "?api_key=" .. appID
 
   if date and date ~= "" then
-    url = url.."&date="..date
+    url = url .. "&date=" .. date
   end
 
   local _, body = http.request("GET", url)
@@ -32,25 +32,32 @@ end
 -- Print an error message is a request is not returned
 local function printError(m, s)
   s = s or ""
-  sendEmbed(m, "Something went wrong :(\n"..s)
+  sendEmbed(m, "Something went wrong :(\n" .. s)
 end
 
 
 command.onCommand = function(m)
   local param = Misc.getParametersLC(m)
   local pattern = "%d%d%d%d%-%d%d%-%d%d"
+  local date = string.match(param, pattern)
 
-  if not string.match(param, pattern) then printError(m, "Date `"..param.."` does not match format YYYY-MM-DD") return end
-
-  local apod = requestAPOD(param)
-  if apod and apod.code == 400 then printError(m, "Date `"..param.."` does not match format YYYY-MM-DD") return end
+  local apod = requestAPOD(date)
+  if apod and apod.code == 400 then printError(m, "Date `" .. date .. "` does not match format YYYY-MM-DD") return end
   if not apod or not apod.explanation then printError(m) return end
 
-  local text = "__"..apod.title.."__ "..(apod.date or "").."\n"
+  local text = "__" .. apod.title .. "__ " .. (apod.date or "") .. "\n"
   if apod.copyright then
-    text = text.."©*"..apod.copyright.."*"
+    text = text .. "©*" .. apod.copyright .. "*\n"
   end
-  sendEmbed(m, {title = "NASA's Astronomy Picture of the Day", text = text, imageURL = (apod.hdurl or apod.url)})
+
+  if apod.media_type == "video" then
+    local videoURL = string.sub(apod.url, 31, #apod.url - 6)
+    videoURL = "https://youtu.be/" .. videoURL
+    m.channel:send("**NASA's Astronomy Picture of the Day**\n" .. text .. videoURL)
+  else
+    sendEmbed(m, {title = "NASA's Astronomy Picture of the Day", text = text, imageURL = apod.hdurl or apod.url})
+  end
+
   sendEmbed(m, {title = "NASA's Astronomy Picture of the Day", text = apod.explanation}) -- Split into two since its too long
 end
 
